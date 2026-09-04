764927f9bda7b3fa7854876c9b81ba51a75be279
ggcrunchy <steve@xibalbastudios.com>
2026-09-04 17:36:49 -0600

    Updated to newest sampler and image types LUT (only one, huzzah) and ClassifySampler(), plus revised notes

--- librtt/Renderer/Rtt_GLProgram.cpp
+++ librtt/Renderer/Rtt_GLProgram.cpp
@@ -612,99 +612,45 @@ GLProgram::UpdateShaderSource( Program* program, Program::Version version, Versi
 #endif
 }
 
-// The following LUTs comprise the inlined image / sampler type constants from OpenGL 4.6 + ES 3.2,
-// along with some metadata. The list used by Slang is taken as the final authority:
+// The below LUT holds useful metadata for the 102 image and sampler types belonging
+// to OpenGL 4.6 + ES 3.2 + extensions. Slang's list is taken as authoritative:
 
 // https://raw.githubusercontent.com/KhronosGroup/glslang/refs/heads/main/glslang/MachineIndependent/gl_types.h
 
-// These 102 16-bit constants fall into four low-byte sequences: 0x8-0xF, 0x4C-0x6C, 0xC0-0xC5, and
-// 0xC9-0xEA. The low bytes themselves fall into the subsequences 0xC-0xD, 0x5D-0x64, and 0xCE-0xD8.
-// As it turns out, a common low byte never occurs in more than two of the constants.
+// The index into this LUT, provided a valid constant, is given by ClassifySampler().
+// Bogus constants or irrelevant uniform types return 0 (the LUT contains an all-0s
+// sentinel metadatum here).
 
-// We can flatten the intervals into an index space: 0x-0xF -> 0-7; 0x4C-0x6C -> 8-40, etc. The
-// calculations can be simplified a bit by merging the third and fourth sequence, and addressing
-// the narrow 0xC6-0xC8 gap in a different way. This almost lets us access the constants, when
-// sorted by low byte.
+// A large swath of constants can be trivially rejected, since only four high bytes
+// are valid: 0x8B, 0x8D, 0x90, and 0x91. The good bytes cover a tiny range, and an
+// offset into it is a 3-bit integer in [0, 6], with 7 a handy sentinel. 
 
-// We need to apply some offseting to account for low bytes belonging to two constants. The first
-// such byte is found at index 4 and the last at 65. This range fits comfortably within a 64-bit
-// integer; by mapping indices to bits, we can use popcount techniques to produce an offset.
+// The valid low bytes are broken up across four ranges: 0x8-0xF, 0x4C-0x6C, 0xC0-
+// 0xC5, and 0xC9-0xEA. There are 81 entries here; certain low bytes show up more
+// than once: subsequences 0xC-0xD, 0x5D-0x64, and 0xCE-0xD8. There are in fact 21
+// such cases, which was exactly the shortfall (102 - 81): a low byte belongs to
+// at most two constants.
 
-// A given constant is looked up by low byte and comapred against the sorted list. Since pairs
-// are always adjacent, we can check both at once.
+// We can flatten these intervals into an index space: 0x-0xF -> 0-7; 0x4C-0x6C ->
+// 8-40, etc. This almost lets us access the constants, when sorted by low byte.
 
-// The sorting puts most constants with a common low byte on an even boundary, i.e. position 0,
-// 2, 4, etc. In fact, a little nudge will make this universal: pretend 0x5C also belongs to a
-// pair, and add dummy LUT entries and low-byte accounting in the proper spots. The later LUT
-// entries move ahead, and every such pair of constants now share a bin. Thus we can find just
-// one bin for a given low byte and try both values at once.
+// We must apply some offseting to account for low byte pairs. This first occurs
+// at index 4 and lastly at 65. These fit comfortably within a 64-bit integer; by
+// mapping indices to bits, we can use popcount techniques to produce an offset.
 
-// In the case of the 0xC6-0xC8 gap and 0x5C dummy, neither considered valid, we only care that
-// the given comparison fails, so any out-of-reach value can be used in the dummy position. Each
-// of those cases is well along in the list, so the very first element is convenient. (As for the
-// metadata, an all-0s constant is likewise bogus.)
+// A given constant is looked up by low byte and compared against the sorted list.
+// Since pairs are always adjacent, we can check both at once. In fact, we can pack
+// two 3-bit high-byte offsets (as described above), with a dummy in the majority
+// no-pair cases, and do SWAR operations.
 
-// An exhaustive trial was run over constants 0-0xFFFF and confirmed that only image and sampler
-// types turn up. Currently, this is used to distinguish these uniforms from atomic counters and
-// numeric types, when enumerated linked uniforms, but also flags invalid values.
+// There are also few enough such 6-bit sequences that they can also be boiled down
+// to 3-bit IDs, which then pack well into 17 16-bit integers. The leading integer,
+// and several of the trailing ones, all consist of the same ID (in the choice used,
+// 0), so some unsigned indexing shenanigans allows further reduction to 12 integers.
 
-#define PACK( lo, hi ) ( ( (U32)( hi ) << 16 ) | (U32)( lo ) )
-
-static const U32 kConstantPairs[] = {
-	PACK( 0x9108, 0x9109 ), /* SAMPLER_MULTISAMPLE, INT_SAMPLER_MULTISAMPLE */
-	PACK( 0x910A, 0x910B ), /* UNSIGNED_INT_SAMPLER_MULTISAMPLE, SAMPLER_MULTISAMPLE_ARRAY */
-	PACK( 0x900C, 0x910C ), /* SAMPLER_CUBE_MAP_ARRAY, INT_SAMPLER_MULTISAMPLE_ARRAY */
-	PACK( 0x910D, 0x900D ), /* UNSIGNED_INT_SAMPLER_MULTISAMPLE_ARRAY, SAMPLER_CUBE_MAP_ARRAY_SHADOW */
-	PACK( 0x900E, 0x900F ), /* INT_SAMPLER_CUBE_MAP_ARRAY, UNSIGNED_INT_SAMPLER_CUBE_MAP_ARRAY */
-	PACK( 0x904C, 0x904D ), /* IMAGE_1D, IMAGE_2D */
-	PACK( 0x904E, 0x904F ), /* IMAGE_3D, IMAGE_RECTANGLE */
-	PACK( 0x9050, 0x9051 ), /* IMAGE_CUBE, IMAGE_BUFFER */
-	PACK( 0x9052, 0x9053 ), /* IMAGE_1D_ARRAY, IMAGE_2D_ARRAY */
-	PACK( 0x9054, 0x9055 ), /* IMAGE_CUBE_MAP_ARRAY, IMAGE_MULTISAMPLE */
-	PACK( 0x9056, 0x9057 ), /* IMAGE_MULTISAMPLE_ARRAY, INT_IMAGE_1D  */
-	PACK( 0x9058, 0x9059 ), /* INT_IMAGE_2D, INT_IMAGE_3D */
-	PACK( 0x905A, 0x905B ), /* INT_IMAGE_RECTANGLE, INT_IMAGE_CUBE */
-	PACK( 0x905C, 0x9108 ), /* INT_IMAGE_BUFFER, SENTINEL */
-	PACK( 0x905D, 0x8B5D ), /* INT_IMAGE_1D_ARRAY, SAMPLER_1D */
-	PACK( 0x8B5E, 0x905E ), /* SAMPLER_2D, INT_IMAGE_2D_ARRAY */
-	PACK( 0x8B5F, 0x905F ), /* SAMPLER_3D, INT_IMAGE_CUBE_MAP_ARRAY */
-	PACK( 0x8B60, 0x9060 ), /* SAMPLER_CUBE, INT_IMAGE_MULTISAMPLE */
-	PACK( 0x9061, 0x8B61 ), /* INT_IMAGE_MULTISAMPLE_ARRAY, SAMPLER_1D_SHADOW */
-	PACK( 0x8B62, 0x9062 ), /* SAMPLER_2D_SHADOW, UNSIGNED_INT_IMAGE_1D */
-	PACK( 0x9063, 0x8B63 ), /* UNSIGNED_INT_IMAGE_2D, SAMPLER_RECT */
-	PACK( 0x8B64, 0x9064 ), /* SAMPLER_RECT_SHADOW, UNSIGNED_INT_IMAGE_3D */
-	PACK( 0x9065, 0x9066 ), /* UNSIGNED_INT_IMAGE_RECTANGLE, UNSIGNED_INT_IMAGE_CUBE */
-	PACK( 0x9067, 0x9068 ), /* UNSIGNED_INT_IMAGE_BUFFER, UNSIGNED_INT_IMAGE_1D_ARRAY */
-	PACK( 0x9069, 0x906A ), /* UNSIGNED_INT_IMAGE_2D_ARRAY, UNSIGNED_INT_IMAGE_CUBE_MAP_ARRAY */
-	PACK( 0x906B, 0x906C ), /* UNSIGNED_INT_IMAGE_MULTISAMPLE, UNSIGNED_INT_IMAGE_MULTISAMPLE_ARRAY */
-	PACK( 0x8DC0, 0x8DC1 ), /* SAMPLER_1D_ARRAY, SAMPLER_2D_ARRAY */
-	PACK( 0x8DC2, 0x8DC3 ), /* SAMPLER_BUFFER, SAMPLER_1D_ARRAY_SHADOW */
-	PACK( 0x8DC4, 0x8DC5 ), /* SAMPLER_2D_ARRAY_SHADOW, SAMPLER_CUBE_SHADOW */
-	PACK( 0x9108, 0x9108 ), /* SENTINEL, SENTINEL */
-	PACK( 0x9108, 0x8DC9 ), /* SENTINEL, INT_SAMPLER_1D */
-	PACK( 0x8DCA, 0x8DCB ), /* INT_SAMPLER_2D, INT_SAMPLER_3D */
-	PACK( 0x8DCC, 0x8DCD ), /* INT_SAMPLER_CUBE, INT_SAMPLER_RECT */
-	PACK( 0x8DCE, 0x91CE ), /* INT_SAMPLER_1D_ARRAY, FLOAT16_SAMPLER_1D */
-	PACK( 0x91CF, 0x8DCF ), /* FLOAT16_SAMPLER_2D, INT_SAMPLER_2D_ARRAY */
-	PACK( 0x91D0, 0x8DD0 ), /* FLOAT16_SAMPLER_3D, INT_SAMPLER_BUFFER */
-	PACK( 0x8DD1, 0x91D1 ), /* UNSIGNED_INT_SAMPLER_1D, FLOAT16_SAMPLER_CUBE */
-	PACK( 0x8DD2, 0x91D2 ), /* UNSIGNED_INT_SAMPLER_2D, FLOAT16_SAMPLER_2D_RECT */
-	PACK( 0x91D3, 0x8DD3 ), /* FLOAT16_SAMPLER_1D_ARRAY, UNSIGNED_INT_SAMPLER_3D */
-	PACK( 0x91D4, 0x8DD4 ), /* FLOAT16_SAMPLER_2D_ARRAY, UNSIGNED_INT_SAMPLER_CUBE */
-	PACK( 0x91D5, 0x8DD5 ), /* FLOAT16_SAMPLER_CUBE_MAP_ARRAY, UNSIGNED_INT_SAMPLER_RECT */
-	PACK( 0x8DD6, 0x91D6 ), /* UNSIGNED_INT_SAMPLER_1D_ARRAY, FLOAT16_SAMPLER_BUFFER */
-	PACK( 0x91D7, 0x8DD7 ), /* FLOAT16_SAMPLER_2D_MULTISAMPLE, UNSIGNED_INT_SAMPLER_2D_ARRAY */
-	PACK( 0x8DD8, 0x91D8 ), /* UNSIGNED_INT_SAMPLER_BUFFER, FLOAT16_SAMPLER_2D_MULTISAMPLE_ARRAY */
-	PACK( 0x91D9, 0x91DA ), /* FLOAT16_SAMPLER_1D_SHADOW, FLOAT16_SAMPLER_2D_SHADOW */
-	PACK( 0x91DB, 0x91DC ), /* FLOAT16_SAMPLER_2D_RECT_SHADOW, FLOAT16_SAMPLER_1D_ARRAY_SHADOW */
-	PACK( 0x91DD, 0x91DE ), /* FLOAT16_SAMPLER_2D_ARRAY_SHADOW, FLOAT16_SAMPLER_CUBE_SHADOW */
-	PACK( 0x91DF, 0x91E0 ), /* FLOAT16_SAMPLER_CUBE_MAP_ARRAY_SHADOW, FLOAT16_IMAGE_1D */
-	PACK( 0x91E1, 0x91E2 ), /* FLOAT16_IMAGE_2D, FLOAT16_IMAGE_3D */
-	PACK( 0x91E3, 0x91E4 ), /* FLOAT16_IMAGE_2D_RECT, FLOAT16_IMAGE_CUBE */
-	PACK( 0x91E5, 0x91E6 ), /* FLOAT16_IMAGE_1D_ARRAY, FLOAT16_IMAGE_2D_ARRAY */
-	PACK( 0x91E7, 0x91E8 ), /* FLOAT16_IMAGE_CUBE_MAP_ARRAY, FLOAT16_IMAGE_BUFFER */
-	PACK( 0x91E9, 0x91EA )  /* FLOAT16_IMAGE_2D_MULTISAMPLE, FLOAT16_IMAGE_2D_MULTISAMPLE_ARRAY */
-};
+// Exhaustive trials, with ClassifySampler() run over constants 0-0xFFFF, confirm
+// that only image and sampler types turn up. Currently the classifier is only used
+// to ignore atomic counters and numeric types when enumerating linked uniforms.
 
 Rtt_STATIC_ASSERT( Texture::kNumFamilies <= ( 1 << 2 ) );
 Rtt_STATIC_ASSERT( Texture::kNumTargets <= ( 1 << 3 ) );
@@ -728,8 +674,8 @@ static const SamplerTypeDetails kDetails[] = {
 	SAMPLER_ARRAY( Multisample ), /* SAMPLER_MULTISAMPLE_ARRAY */
 	SAMPLER_ARRAY( Cube ), /* SAMPLER_CUBE_MAP_ARRAY */
 	INTEGER_SAMPLER_ARRAY( Signed, Multisample ), /* INT_SAMPLER_MULTISAMPLE_ARRAY */
-	INTEGER_SAMPLER_ARRAY( Unsigned, Multisample ), /* UNSIGNED_INT_SAMPLER_MULTISAMPLE_ARRAY */
 	SHADOW_SAMPLER_ARRAY( Cube ), /* SAMPLER_CUBE_MAP_ARRAY_SHADOW */
+	INTEGER_SAMPLER_ARRAY( Unsigned, Multisample ), /* UNSIGNED_INT_SAMPLER_MULTISAMPLE_ARRAY */
 	INTEGER_SAMPLER_ARRAY( Signed, Cube ), /* INT_SAMPLER_CUBE_MAP_ARRAY */
 	INTEGER_SAMPLER_ARRAY( Unsigned, Cube ), /* UNSIGNED_INT_SAMPLER_CUBE_MAP_ARRAY */
 	IMAGE( 1D ), /* IMAGE_1D */
@@ -743,27 +689,26 @@ static const SamplerTypeDetails kDetails[] = {
 	IMAGE_ARRAY( Cube ), /* IMAGE_CUBE_MAP_ARRAY */
 	IMAGE( Multisample ), /* IMAGE_MULTISAMPLE */
 	IMAGE_ARRAY( Multisample ), /* IMAGE_MULTISAMPLE_ARRAY */
-	INTEGER_IMAGE( Signed, 1D ), /* INT_IMAGE_1D  */
+	INTEGER_IMAGE( Signed, 1D ), /* INT_IMAGE_1D */
 	INTEGER_IMAGE( Signed, 2D ), /* INT_IMAGE_2D */
 	INTEGER_IMAGE( Signed, 3D ), /* INT_IMAGE_3D */
 	INTEGER_IMAGE( Signed, Rectangle ), /* INT_IMAGE_RECTANGLE */
 	INTEGER_IMAGE( Signed, Cube ), /* INT_IMAGE_CUBE */
 	INTEGER_IMAGE( Signed, Buffer ), /* INT_IMAGE_BUFFER */
-	{ /* SENTINEL */ },
-	INTEGER_IMAGE_ARRAY( Signed, 1D ), /* INT_IMAGE_1D_ARRAY */
 	SAMPLER( 1D ), /* SAMPLER_1D */
+	INTEGER_IMAGE_ARRAY( Signed, 1D ), /* INT_IMAGE_1D_ARRAY */
 	SAMPLER( 2D ), /* SAMPLER_2D */
 	INTEGER_IMAGE_ARRAY( Signed, 2D ), /* INT_IMAGE_2D_ARRAY */
 	SAMPLER( 3D ), /* SAMPLER_3D */
 	INTEGER_IMAGE_ARRAY( Signed, Cube ), /* INT_IMAGE_CUBE_MAP_ARRAY */
 	SAMPLER( Cube ), /* SAMPLER_CUBE */
 	INTEGER_IMAGE( Signed, Multisample ), /* INT_IMAGE_MULTISAMPLE */
-	INTEGER_IMAGE_ARRAY( Signed, Multisample ), /* INT_IMAGE_MULTISAMPLE_ARRAY */
 	SHADOW_SAMPLER( 1D ), /* SAMPLER_1D_SHADOW */
+	INTEGER_IMAGE_ARRAY( Signed, Multisample ), /* INT_IMAGE_MULTISAMPLE_ARRAY */
 	SHADOW_SAMPLER( 2D ), /* SAMPLER_2D_SHADOW */
 	INTEGER_IMAGE( Unsigned, 1D ), /* UNSIGNED_INT_IMAGE_1D */
-	INTEGER_IMAGE( Unsigned, 2D ), /* UNSIGNED_INT_IMAGE_2D */
 	SAMPLER( Rectangle ), /* SAMPLER_RECT */
+	INTEGER_IMAGE( Unsigned, 2D ), /* UNSIGNED_INT_IMAGE_2D */
 	SHADOW_SAMPLER( Rectangle ), /* SAMPLER_RECT_SHADOW */
 	INTEGER_IMAGE( Unsigned, 3D ), /* UNSIGNED_INT_IMAGE_3D */
 	INTEGER_IMAGE( Unsigned, Rectangle ), /* UNSIGNED_INT_IMAGE_RECTANGLE */
@@ -774,15 +719,12 @@ static const SamplerTypeDetails kDetails[] = {
 	INTEGER_IMAGE_ARRAY( Unsigned, Cube ), /* UNSIGNED_INT_IMAGE_CUBE_MAP_ARRAY */
 	INTEGER_IMAGE( Unsigned, Multisample ), /* UNSIGNED_INT_IMAGE_MULTISAMPLE */
 	INTEGER_IMAGE_ARRAY( Unsigned, Multisample ), /* UNSIGNED_INT_IMAGE_MULTISAMPLE_ARRAY */
-	SAMPLER_ARRAY( 1D ), /* SAMPLER_1D_ARRAY	 */
+	SAMPLER_ARRAY( 1D ), /* SAMPLER_1D_ARRAY */ 
 	SAMPLER_ARRAY( 2D ), /* SAMPLER_2D_ARRAY */
 	SAMPLER( Buffer ), /* SAMPLER_BUFFER */
 	SHADOW_SAMPLER_ARRAY( 1D ), /* SAMPLER_1D_ARRAY_SHADOW */
 	SHADOW_SAMPLER_ARRAY( 2D ), /* SAMPLER_2D_ARRAY_SHADOW */
 	SHADOW_SAMPLER( Cube ), /* SAMPLER_CUBE_SHADOW */
-	{ /* SENTINEL */ },
-	{ /* SENTINEL */ },
-	{ /* SENTINEL */ },
 	INTEGER_SAMPLER( Signed, 1D ), /* INT_SAMPLER_1D */
 	INTEGER_SAMPLER( Signed, 2D ), /* INT_SAMPLER_2D */
 	INTEGER_SAMPLER( Signed, 3D ), /* INT_SAMPLER_3D */
@@ -790,24 +732,24 @@ static const SamplerTypeDetails kDetails[] = {
 	INTEGER_SAMPLER( Signed, Rectangle ), /* INT_SAMPLER_RECT */
 	INTEGER_SAMPLER_ARRAY( Signed, 1D ), /* INT_SAMPLER_1D_ARRAY */
 	SAMPLER( 1D ), /* FLOAT16_SAMPLER_1D */
-	SAMPLER( 2D ), /* FLOAT16_SAMPLER_2D */
 	INTEGER_SAMPLER_ARRAY( Signed, 2D ), /* INT_SAMPLER_2D_ARRAY */
-	SAMPLER( 3D ), /* FLOAT16_SAMPLER_3D */
+	SAMPLER( 2D ), /* FLOAT16_SAMPLER_2D */
 	INTEGER_SAMPLER( Signed, Buffer ), /* INT_SAMPLER_BUFFER */
+	SAMPLER( 3D ), /* FLOAT16_SAMPLER_3D */
 	INTEGER_SAMPLER( Unsigned, 1D ), /* UNSIGNED_INT_SAMPLER_1D */
 	SAMPLER( Cube ), /* FLOAT16_SAMPLER_CUBE */
 	INTEGER_SAMPLER( Unsigned, 2D ), /* UNSIGNED_INT_SAMPLER_2D */
-	SAMPLER( 2D ), /* FLOAT16_SAMPLER_2D_RECT */
-	SAMPLER_ARRAY( 1D ), /* FLOAT16_SAMPLER_1D_ARRAY */
+	SAMPLER( Rectangle ), /* FLOAT16_SAMPLER_2D_RECT */
 	INTEGER_SAMPLER( Unsigned, 3D ), /* UNSIGNED_INT_SAMPLER_3D */
-	SAMPLER_ARRAY( 2D ), /* FLOAT16_SAMPLER_2D_ARRAY */
+	SAMPLER_ARRAY( 1D ), /* FLOAT16_SAMPLER_1D_ARRAY */
 	INTEGER_SAMPLER( Unsigned, Cube ), /* UNSIGNED_INT_SAMPLER_CUBE */
-	SAMPLER_ARRAY( Cube ), /* FLOAT16_SAMPLER_CUBE_MAP_ARRAY */
+	SAMPLER_ARRAY( 2D ), /* FLOAT16_SAMPLER_2D_ARRAY */
 	INTEGER_SAMPLER( Unsigned, Rectangle ), /* UNSIGNED_INT_SAMPLER_RECT */
+	SAMPLER_ARRAY( Cube ), /* FLOAT16_SAMPLER_CUBE_MAP_ARRAY */
 	INTEGER_SAMPLER_ARRAY( Unsigned, 1D ), /* UNSIGNED_INT_SAMPLER_1D_ARRAY */
 	SAMPLER( Buffer ), /* FLOAT16_SAMPLER_BUFFER */
-	SAMPLER( Multisample ), /* FLOAT16_SAMPLER_2D_MULTISAMPLE */
 	INTEGER_SAMPLER_ARRAY( Unsigned, 2D ), /* UNSIGNED_INT_SAMPLER_2D_ARRAY */
+	SAMPLER( Multisample ), /* FLOAT16_SAMPLER_2D_MULTISAMPLE */
 	INTEGER_SAMPLER( Unsigned, Buffer ), /* UNSIGNED_INT_SAMPLER_BUFFER  */
 	SAMPLER_ARRAY( Multisample ), /* FLOAT16_SAMPLER_2D_MULTISAMPLE_ARRAY */
 	SHADOW_SAMPLER( 1D ), /* FLOAT16_SAMPLER_1D_SHADOW */
@@ -842,40 +784,72 @@ static const SamplerTypeDetails kDetails[] = {
 #undef SHADOW_SAMPLER_ARRAY
 
 #define COUNT_IN_RANGE( low, high ) ( high - low + 1 )
-#define GET_OFFSET_IN_RANGE( low, high, base, value ) ( ( ( (U8)( value - low ) ) <= (U8)( high - low ) ) ? ( base + value - low ) : 0 )
-#define SPLAT( v ) PACK( v, v )
+#define LOW_TO_HIGH( low, high ) ( (uint8_t)( high - low ) )
+#define COMP_RANGES( low, high, value ) ( LOW_TO_HIGH( low, value ) <= LOW_TO_HIGH( low, high ) )
+#define GET_OFFSET_IN_RANGE( low, high, base, value ) ( ( base + LOW_TO_HIGH( low, value ) ) & -COMP_RANGES( low, high, value ) )
+
+#define SPLAT( v ) ( (uint8_t)( ( v ) | ( ( v ) << 3 ) ) )
 
 static int
 ClassifySampler( GLenum type )
 {
-	const int Base1 = 0;
+	// Valid constants only have high bytes 0x8B, 0x8D, 0x90, and 0x91,
+	// and these can use delta from 0x8B as an index into a LUT byte as
+	// validity check. Similar interval-based work gives a flat offset.
+	uint8_t delta = (uint8_t)( ( type >> 8 ) - 0x8B );
+	uint8_t valid = ( delta <= 6 ) & ( 0x65 /* 01100101b */ >> ( delta & 7 ) );
+	uint8_t low = (uint8_t)( type & -valid );
+
+	const int Base1 = 1;
 	const int Base2 = Base1 + COUNT_IN_RANGE( 0x8, 0xF );
 	const int Base3 = Base2 + COUNT_IN_RANGE( 0x4C, 0x6C );
-
-	GLenum low = type & 0xFF;
+	const int Base4 = Base3 + COUNT_IN_RANGE( 0xC0, 0xC5 );
+	
 	int offset = GET_OFFSET_IN_RANGE( 0x8, 0xF, Base1, low ) |
 				GET_OFFSET_IN_RANGE( 0x4C, 0x6C, Base2, low ) |
-				GET_OFFSET_IN_RANGE( 0xC0, 0xEA, Base3, low );
+				GET_OFFSET_IN_RANGE( 0xC0, 0xC5, Base3, low ) |
+				GET_OFFSET_IN_RANGE( 0xC9, 0xEA, Base4, low );
 
-	// When we sort the values by low byte, the first one with two entries is at
-	// index 4, and the last at index 65. With a slight offset this fits neatly
-	// into a 64-bit integer, and clamping at either end satisfies empty or full
-	// outside the range. (There is a false pair for 0x5C; it has one legitimate
-	// full value, and also a sentinel. With that done, however, each proper one
-	// starts at an even offset, and thus both values pair up.)
-	int shift = Min( Max( offset - 4, 0 ), 63 );
-	uint64_t mask = ( 1ULL << shift ) - 1;
+	// Each uint16_t holds 5 3-bit IDs (0-6), representing some common
+	// patterns. There are 81 unique low bytes plus offset 0 to consider,
+	// so implicitly this comprises ceil(82 / 5) = 17 uint16_t values.
+	// As it happens, the first four IDs are 0, and the sentinel can be
+	// as well; several trailing IDs are as well. The form shown here
+	// uses some indexing adjustments to lop off the dead weight.
+	const uint16_t kPatternIDs[] = {
+		0x2489, 0x2492, 0x2492, 0x2492, 0x36DA, 0x26DB,
+		0x2492, 0x4912, 0x4924, 0x5B24, 0x5B6D, 0x0B6D
+	};
 
-	offset += __builtin_popcountll( 0x3FF800001FF00003ULL & mask ); // TODO: Windows
+	uint8_t ids_index = (uint8_t)( offset / 5 - 1 ), shift = ( offset % 5 ) * 3;
+	uint64_t id = ids_index <= 11 ? ( kPatternIDs[ids_index] >> shift ) & 7 : 0;
 
-	// Two-element SWAR to check both values in pair.
-	uint32_t diff = kConstantPairs[offset / 2] ^ SPLAT( type );
-    uint32_t hits = ( diff - SPLAT( 0x0001 ) ) & ~diff & SPLAT( 0x8000 );
+	// Find the ID's pattern, which consists of two 3-bit high byte
+	// deltas, indicating what may validly pair with the low byte.
+	// (Most cases only support one pairing, with the second delta
+	// being the sentinel, 7.)
+	uint8_t pair = ( 0x3FCBAA3DD7EULL >> ( id * 6 ) ) & 63;
 
-	return hits ? ( offset & ~1 ) + ( 0x80000000UL == hits ) + 1 : 0;
+	// Check both at once with a SWAR operation.
+	uint8_t diff = pair ^ SPLAT(delta);
+    uint8_t hits = (diff - SPLAT(0x1)) & ~diff & SPLAT(0x4) & -valid;
+
+	// There is a stretch of the flat offset space, < 64 in length,
+	// where the low bytes with two entries occur, so this can be
+	// captured in a mask. Account for these pairs in the final
+	// offset, and also invalidate the result on a bad high byte.
+	uint64_t mask = ( 1ULL << Min( Max( offset - 5, 0 ), 63 ) ) - 1;
+
+	offset += __builtin_popcountll( 0x7FF00001FE00003ULL & mask );
+	offset &= -( hits > 0 );
+
+	// Supply the offset, which might be the second in a pair.
+	return offset + ( 0x20 == hits );
 }
 
 #undef COUNT_IN_RANGE
+#undef LOW_TO_HIGH
+#undef COMP_RANGES
 #undef GET_OFFSET_IN_RANGE
 #undef SPLAT
 #undef PACK
@@ -892,7 +866,7 @@ IsBuiltInSampler( GLchar* buf )
 		char _[8];
 		U64 u64;
 		
-		bool operator == (U64 other) const { return u64 == other; }
+		bool operator==( U64 other ) const { return u64 == other; }
 	}
 
 	// first half:
@@ -909,8 +883,8 @@ IsBuiltInSampler( GLchar* buf )
 	
 	// Reinterpret buf as two U64s. It should be sufficiently aligned that a
 	// cast would also work, but compilers seem to smile on the idiom here.
-	memcpy(&name1, buf, sizeof(U64));
-	memcpy(&name2, buf + sizeof(U64), sizeof(U64));
+	memcpy( &name1, buf, sizeof(U64) );
+	memcpy( &name2, buf + sizeof(U64), sizeof(U64) );
 
 	U64 digit = kWipeJunk.u64 & name2;
 
